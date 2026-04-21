80e6c9011fe28135e9fab5723301cc91c72748c2
rain-lua <rainroblox9@gmail.com>
2026-04-21 21:11:27 +0200

    remove forward declaration

--- src/core/output/MonitorManager.hpp
+++ src/core/output/MonitorManager.hpp
@@ -3,12 +3,10 @@
 
 #include "../../include/Defines.hpp"
 
-class Compositor;
-
 struct Monitor {
     wl_list m_Link;
-
     wlr_output *m_WlrOutput;
+
     wl_listener m_Frame;
     wl_listener m_RequestState;
     wl_listener m_Destroy;
